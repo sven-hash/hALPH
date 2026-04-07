bc40ff5204cba8f7e6829dffa11d5f5162194f59
sven-hash <>
2026-04-07 19:58:14 +0200

    new: frontend

---
+++ deployments.d.ts
@@ -0,0 +1,14 @@
+declare module './deployments/.deployments.testnet.json' {
+  const deployments: any[]
+  export default deployments
+}
+
+declare module '../deployments/.deployments.testnet.json' {
+  const deployments: any[]
+  export default deployments
+}
+
+declare module '../../deployments/.deployments.testnet.json' {
+  const deployments: any[]
+  export default deployments
+}
